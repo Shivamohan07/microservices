6f7ed4f73cf0887ddfb995350fdaadcf224193fb
legit_shivam <shivamohan07@gmail.com>
2020-04-04 23:19:36 +0530

    Adding a sample api

--- src/main/java/com/impromptu/microservices/MicroservicesApplication.java
+++ src/main/java/com/impromptu/microservices/MicroservicesApplication.java
@@ -2,12 +2,21 @@ package com.impromptu.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+
+	@RequestMapping(value="/greeting",method = RequestMethod.GET)
+	public String greet(){
+			return "Greetings";
 	}
 
 }
