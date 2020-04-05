252f843473a8ceb09dd701c1ad42fec8abdc1b17
shivmoha <shivamohan07@gmail.com>
2020-04-05 18:57:07 +0530

    Adding security

--- src/main/java/com/impromptu/microservices/MicroServicesConfigServerGit.java
+++ src/main/java/com/impromptu/microservices/MicroServicesConfigServerGit.java
@@ -2,11 +2,15 @@ package com.impromptu.microservices;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class MicroServicesConfigServerGit {
     public static void main(String[] args) {
         SpringApplication.run(MicroServicesConfigServerGit.class, args);
--- src/main/java/com/impromptu/microservices/MicroservicesApplication.java
+++ src/main/java/com/impromptu/microservices/MicroservicesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
 @RestController
 public class MicroservicesApplication {
 
